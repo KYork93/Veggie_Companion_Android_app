64a59f707ac047324a97d782280c5e2879de41b3
Kristie Yorkston <kyorkston@gmail.com>
2017-03-17 18:06:57 +0000

    work google maps api

--- Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/RestaurantMainActivity.java
+++ Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/RestaurantMainActivity.java
@@ -1,13 +1,68 @@
 package example.codeclan.com.veggiecompanion;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class RestaurantMainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiActivity;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class RestaurantMainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurants_main);
+        if(googleServicesAvailable()){
+            Toast.makeText(this, "connected", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_restaurants_main);
+            initMap();
+        } else {
+            //No google maps layout
+        }
+
+
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    public boolean googleServicesAvailable(){
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+
+        if(isAvailable == ConnectionResult.SUCCESS){
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)){
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "can't connect to play services", Toast.LENGTH_LONG).show();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        goToLocation(55.95029,-3.205775, 14);
+    }
+
+    private void goToLocation(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        mGoogleMap.moveCamera(update);
     }
 }
