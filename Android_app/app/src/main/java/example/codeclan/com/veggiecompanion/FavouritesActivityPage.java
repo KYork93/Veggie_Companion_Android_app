8b84d801c320bd1a96e3307859d6d965fa777a07
Kristie Yorkston <kyorkston@gmail.com>
2017-03-20 17:42:25 +0000

    add menu to favourites page

--- Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/FavouritesActivityPage.java
+++ Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/FavouritesActivityPage.java
@@ -1,7 +1,11 @@
 package example.codeclan.com.veggiecompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
@@ -11,6 +15,7 @@ import android.widget.TextView;
 public class FavouritesActivityPage extends AppCompatActivity{
 
     TextView textView;
+    Intent intent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,21 @@ public class FavouritesActivityPage extends AppCompatActivity{
 
         textView = (TextView)findViewById(R.id.favourite_item);
         textView.setText("awright");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.home){
+            intent = new Intent(FavouritesActivityPage.this, RestaurantMainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
