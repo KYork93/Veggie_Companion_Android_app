a8009b5a9d364a5b7cd6971e510657d8b5e65846
Kristie Yorkston <kyorkston@gmail.com>
2017-03-20 13:23:37 +0000

    edit showing list

--- Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/RestaurantMainActivity.java
+++ Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/RestaurantMainActivity.java
@@ -22,6 +22,7 @@ import com.google.android.gms.maps.model.LatLng;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantMainActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -40,23 +41,7 @@ public class RestaurantMainActivity extends AppCompatActivity implements OnMapRe
             //No google maps layout
         }
 
-        DBHandler db = new DBHandler(this);
-        db.deleteAllRestaurant();
-
-        db.addToRestaurantTable(new RestaurantModel("Paradise Palms", "41 Lothian St, Edinburgh EH1 1HB", "Tropical house themed restaurant/cocktail bar with excellent vegan options,\n" +
-                "whole restaurant is vegetarian so you can be assured there's no kitchen contamination,\n" +
-                "2 for 1 vegan hotdogs every Tuesday and Vegan Roasts every Sunday.", 0, 55.946272, -3.189225));
-
-        List<RestaurantModel> allRestaurants = db.getAllRestaurants();
-
-        for(RestaurantModel restaurant : allRestaurants){
-            String log = " Name: " + restaurant.getName() + " Address " + restaurant.getAddress();
-            Log.d("Seeding: ", log);
-        }
-
-        restaurantList = (ListView) findViewById(R.id.restaurant_list);
-
-        restaurantList.setAdapter(new ArrayAdapter<RestaurantModel>(this, android.R.layout.simple_list_item_1, allRestaurants));
+        this.addToRestaurantDB();
     }
 
     private void initMap() {
@@ -93,25 +78,25 @@ public class RestaurantMainActivity extends AppCompatActivity implements OnMapRe
     }
 
 
-//    public void addToRestaurantDB(){
-//        DBHandler db = new DBHandler(this);
-//        db.deleteAllRestaurant();
-//
-//        db.addToRestaurantTable(new RestaurantModel("Paradise Palms", "41 Lothian St, Edinburgh EH1 1HB", "Tropical house themed restaurant/cocktail bar with excellent vegan options,\n" +
-//                "whole restaurant is vegetarian so you can be assured there's no kitchen contamination,\n" +
-//                "2 for 1 vegan hotdogs every Tuesday and Vegan Roasts every Sunday.", 0, 55.946272, -3.189225));
-//
-//        List<RestaurantModel> allRestaurants = db.getAllRestaurants();
-//
-//        for(RestaurantModel restaurant : allRestaurants){
-//            String log = " Name: " + restaurant.getName() + " Address " + restaurant.getAddress();
-//            Log.d("Seeding: ", log);
-//        }
-//
-//        restaurantList = (ListView) findViewById(R.id.restaurant_list);
-//
-//        restaurantList.setAdapter(new ArrayAdapter<RestaurantModel>(this, android.R.layout.simple_list_item_1, allRestaurants));
-//
-//    }
+    public void addToRestaurantDB(){
+        DBHandler db = new DBHandler(this);
+        db.deleteAllRestaurant();
+
+        db.addToRestaurantTable(new RestaurantModel("Paradise Palms", "41 Lothian St, Edinburgh EH1 1HB", "Tropical house themed restaurant/cocktail bar with excellent vegan options,\n" +
+                "whole restaurant is vegetarian so you can be assured there's no kitchen contamination,\n" +
+                "2 for 1 vegan hotdogs every Tuesday and Vegan Roasts every Sunday.", 0, 55.946272, -3.189225));
+
+        List<RestaurantModel> allRestaurants = db.getAllRestaurants();
+
+        for(RestaurantModel restaurant : allRestaurants){
+            String log = " Name: " + restaurant.getName() + " Address " + restaurant.getAddress();
+            Log.e("Seeding: ", log);
+        }
+
+        restaurantList = (ListView) findViewById(R.id.restaurant_list);
+
+        restaurantList.setAdapter(new ArrayAdapter<RestaurantModel>(this, android.R.layout.simple_list_item_1, allRestaurants));
+
+    }
 
 }
