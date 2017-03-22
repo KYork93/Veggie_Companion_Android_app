52f9158cd9aeaf55432acdeabfc9e2a34282e807
Kristie Yorkston <kyorkston@gmail.com>
2017-03-22 11:51:55 +0000

    add google maps test

---
+++ Android_app/app/src/androidTest/java/example/codeclan/veggiecompanion/RestaurantActivityInstrumentationTest.java
@@ -0,0 +1,44 @@
+package example.codeclan.veggiecompanion;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import example.codeclan.com.veggiecompanion.R;
+import example.codeclan.com.veggiecompanion.RestaurantMainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withTagKey;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by user on 22/03/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class RestaurantActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<RestaurantMainActivity> activityTestRule =
+            new ActivityTestRule<>(RestaurantMainActivity.class);
+
+    @Test
+    public void checkHeaderIsCorrect(){
+        onView(withText("Veggie Companion")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkForGoogleMaps(){
+        withTagKey(R.string.google_map).matches(onView(withId(R.id.map_fragment)));
+    }
+
+}
