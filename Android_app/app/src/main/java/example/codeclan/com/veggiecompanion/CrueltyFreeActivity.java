321c1daebe2fc31671eee539b28e13effb84d0b6
Kristie Yorkston <kyorkston@gmail.com>
2017-03-22 13:58:32 +0000

    add some images:

--- Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/CrueltyFreeActivity.java
+++ Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/CrueltyFreeActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by user on 22/03/2017.
@@ -18,11 +21,11 @@ public class CrueltyFreeActivity extends AppCompatActivity{
 
     //seeds
     CrueltyFreeModel nyx = new CrueltyFreeModel("NYX", "Nyx is an affordable make up brand who are increasing in popularity due to their cruelty free and mainly vegan status.  The quality of their make up is high and has not affected their price, their colours are highly pigmented and lipsticks last longer than the high end brands.",
-            "cosmetics", 0, R.id.nyx_banner);
+            "cosmetics", 0, R.mipmap.nyxbanner);
     CrueltyFreeModel lush = new CrueltyFreeModel("Lush", "This high street store specialises in making their own products, totally\n" +
             "vegetarian with a lot of vegan products from bath bombs to make up.  There's something " +
             "for everyone here and the staff are friendly, often you can ask for take away samples." +
-            "They have now also opened spas.", "cosmetics", 0, R.id.lush_banner);
+            "They have now also opened spas.", "cosmetics", 0, R.mipmap.lush_cosmetics_banner);
     CrueltyFreeModel ecover = new CrueltyFreeModel("Ecover", "This company produces household cleaning products from washing up liquid\n" +
             "to bathroom cleaner using natural based products which will not harm the environment, no\n" +
             "animal testing is involved they are stored in recycled plastic bottles and even the factory\n" +
@@ -40,6 +43,8 @@ public class CrueltyFreeActivity extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cruelty_free_activity);
+
+        this.showAllCFCompanies();
     }
 
     public void addToTheCrueltyFreeDatabase(){
@@ -53,6 +58,19 @@ public class CrueltyFreeActivity extends AppCompatActivity{
     }
 
     public void showAllCFCompanies(){
+        addToTheCrueltyFreeDatabase();
+        CrueltyFreeDBHandler db = new CrueltyFreeDBHandler(this);
+
+        ArrayList<CrueltyFreeModel> allCompanies = db.getAllCrueltyFree();
+
+        for(CrueltyFreeModel company : allCompanies){
+            String log = "ID: " + company.getId() + " Name: " + company.getName();
+            Log.d("Seeding: ", log);
+        }
+
+        companyList = (ListView) findViewById(R.id.cruelty_free_list);
+        CrueltyFreeAdapter adapter = new CrueltyFreeAdapter(this, R.id.company_name, allCompanies);
+        companyList.setAdapter(adapter);
 
     }
 
