7a23c46050cf4453421fa84b61d8231353ca05a2
Kristie Yorkston <kyorkston@gmail.com>
2017-03-22 14:34:41 +0000

    convert images to png

--- Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/CrueltyFreeDBHandler.java
+++ Android_app/app/src/main/java/example/codeclan/com/veggiecompanion/CrueltyFreeDBHandler.java
@@ -47,7 +47,6 @@ public class CrueltyFreeDBHandler extends SQLiteOpenHelper {
     }
 
     public void addToCrueltyFreeTable(CrueltyFreeModel company){
-
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
